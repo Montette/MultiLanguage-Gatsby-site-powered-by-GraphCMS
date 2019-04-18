3ca5e957e87acb9d62c08eec6ab99d746bb54aed
Monika Radosiewicz <monika.radosiewicz@gmail.com>
2019-04-18 21:40:50 +0200

    add contact page

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,6 +6,8 @@ import Link from '../components/Link';
 
 const Button = (props) => {
     return (
+        props.type === 'submit' ?
+        <button type="submit" className={props.style === 'white' ? cx(styles.button, styles.white) : styles.button }><FormattedMessage id={props.text} /></button> :
         <Link to={props.href} className={props.style === 'white' ? cx(styles.button, styles.white) : styles.button }>{props.cms ? props.text : <FormattedMessage id={props.text} />} </Link>
     )
 };
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,25 +1,63 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
-
-import withLayout from '../layout';
+import { graphql } from 'gatsby'
+import withLayout from '../layout'
 import Link from '../components/Link';
+import Image from '../components/Image';
+import HomeHero from '../components/HomeHero'
+import styles from '../styles/contact.module.scss'
+import Button from '../components/Button'
+import GraphImg from 'graphcms-image'
+import cx from 'classnames'
+import SectionHeading from '../components/SectionHeading'
+import needle from '../images/needle.svg'
+import diddly from '../images/nic.svg'
+import Section from '../components/Section'
+// import portfolioImg from '../images/services2.jpg'
 
-const SecondPage = () => (
+const ContactPage = () => {
+  return (
   <>
-    <h1>
-      <FormattedMessage id="page2.Hi from the second page" />
-    </h1>
-    <p>
-      <FormattedMessage id="page2.Welcome to page 2" />
-    </p>
-    <Link to="/">
-      <FormattedMessage id="page2.Go back to the homepage" />
-    </Link>
+  <main className='main'>
+  <SectionHeading subtitle='home.Read' title='home.What we can do for you' />
+  <div className={styles.container}>
+  <form name="contact"
+        method="post"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+        className={styles.form} id="form">
+				<div className={styles.form__col}>
+					<input type="text" className={styles.form__input} name="name" id="name" required/>
+					<label htmlFor="name" className={styles.form__label}>Imię i nazwisko</label>
+				</div>
+				<div className={styles.form__col}>
+					<input type="text" className={styles.form__input} name="email" id="email" required/>
+					<label htmlFor="email" className={styles.form__label}>Adres e-mail</label>
+				</div>
+				<div className={styles.form__col}>
+					<input type="text" className={styles.form__input} name="phone" id="phone" required/>
+					<label htmlFor="phone" className={styles.form__label}>Numer telefonu</label>
+				</div>
+				<div className={cx(styles.form__col, styles.textarea)}>
+					<textarea className={cx(styles.form__input, styles.textarea)} name="message" id="message" required></textarea>
+					<label htmlFor="message" className={cx(styles.form__label, styles.textarea)}>Jak możemy pomóc?</label>
+				</div>
+				<div className={cx(styles.form__col, styles.form__button)}>
+               <Button  text="Wyślij wiadomość" type="submit"/>
+				</div>
+
+			</form>
+  </div>
+
+  </main>
   </>
-);
+)};
 
 const customProps = {
-  localeKey: 'page2',
+  localeKey: 'contact', // same as file name in src/i18n/translations/your-lang/index.js
 };
 
-export default withLayout(customProps)(SecondPage);
+
+
+
+export default withLayout(customProps)(ContactPage);
