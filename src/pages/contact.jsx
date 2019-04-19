476ff4d86a614b4a0a002cc9bc1c486c1964ca04
Monika Radosiewicz <monika.radosiewicz@gmail.com>
2019-04-19 22:43:10 +0200

    add handling form

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { FormattedMessage } from 'react-intl';
 import { graphql } from 'gatsby'
 import withLayout from '../layout'
@@ -15,32 +15,58 @@ import diddly from '../images/nic.svg'
 import Section from '../components/Section'
 // import portfolioImg from '../images/services2.jpg'
 
-const ContactPage = () => {
+class ContactPage extends Component  {
+
+	state = {
+		name: '',
+		phone: '',
+		email: '',
+		message: ''
+	}
+	submitHandler = (event) => {
+		event.preventDefault();
+		console.log(this.state);
+		fetch('https://usebasin.com/f/1fa9d4146a4f', {
+			method: 'post',
+			headers: {
+				"Content-type": "application/json"
+			},
+			body: JSON.stringify(this.state)
+		})
+		.then(res => res.json())
+		.then(res => {
+			console.log(res);
+		})
+	}
+
+	onInputHandler = (event) => {
+		this.setState({
+			[event.target.name]: event.target.value
+		})
+	}
+
+	render() {
   return (
   <>
   <main className='main'>
   <SectionHeading subtitle='home.Read' title='home.What we can do for you' />
   <div className={styles.container}>
-  <form name="contact"
-        method="post"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-        className={styles.form} id="form">
-        <input type="hidden" name="form-name" value="contact" />
+  <form className={styles.form} id="form" onSubmit={this.submitHandler}>
+
 				<div className={styles.form__col}>
-					<input type="text" className={styles.form__input} name="name" id="name" required/>
+					<input type="text" className={styles.form__input} name="name" id="name" required value={this.state.name} onChange={this.onInputHandler}/>
 					<label htmlFor="name" className={styles.form__label}>Imię i nazwisko</label>
 				</div>
 				<div className={styles.form__col}>
-					<input type="text" className={styles.form__input} name="email" id="email" required/>
+					<input type="text" className={styles.form__input} name="email" id="email" required value={this.state.email} onChange={this.onInputHandler}/>
 					<label htmlFor="email" className={styles.form__label}>Adres e-mail</label>
 				</div>
 				<div className={styles.form__col}>
-					<input type="text" className={styles.form__input} name="phone" id="phone" required/>
+					<input type="text" className={styles.form__input} name="phone" id="phone" required value={this.state.phone} onChange={this.onInputHandler}/>
 					<label htmlFor="phone" className={styles.form__label}>Numer telefonu</label>
 				</div>
 				<div className={cx(styles.form__col, styles.textarea)}>
-					<textarea className={cx(styles.form__input, styles.textarea)} name="message" id="message" required></textarea>
+					<textarea className={cx(styles.form__input, styles.textarea)} name="message" id="message" required value={this.state.message} onChange={this.onInputHandler}></textarea>
 					<label htmlFor="message" className={cx(styles.form__label, styles.textarea)}>Jak możemy pomóc?</label>
 				</div>
 				<div className={cx(styles.form__col, styles.form__button)}>
@@ -52,7 +78,8 @@ const ContactPage = () => {
 
   </main>
   </>
-)};
+)}
+  };
 
 const customProps = {
   localeKey: 'contact', // same as file name in src/i18n/translations/your-lang/index.js
